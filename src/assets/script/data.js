784abbf79fc5f87a73be3166c73149b1fd032438
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-03-23 09:30:01 +0100

    add some scss & clean folder

---
+++ src/assets/script/data.js
@@ -0,0 +1,74 @@
+export default [
+  {
+  id: 1,
+  title: ' ',
+  path: '/src/img/deathshroud-terminator-01.jpg',
+  keywords: [' ', ' ', ' ', ' '], 
+  },
+  {
+  id: 2,
+  title: ' ',
+  path: '/src/img/deathshroud-terminator-02.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 3,
+  title: ' ',
+  path: '/src/img/deathshroud-terminators.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 4,
+  title: ' ',
+  path: '/src/img/plague-marine.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 5,
+  title: ' ',
+  path: '/src/img/plague-marines-01.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 6,
+  title: ' ',
+  path: '/src/img/plague-marines-02.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 7,
+  title: ' ',  // Space Marine MK VI Imperial Fist
+  path: '/src/img/space-marine-mk6-if.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 8,
+  title: ' ', // Space Marines MKVI
+  path: '/src/img/space-marines-mk6.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 9,
+  title: ' ', // Ultramarines
+  path: '/src/img/ultramarines.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 10,
+  title: 'Vargskyr',
+  path: '/src/img/vargskyr.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 11,
+  title: 'Gorslav The Gravekeeper',
+  path: '/src/img/gorslave-the-gravekeeper.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+  {
+  id: 12,
+  title: ' ', // Deadwalkers Zombies
+  path: '/src/img/deadwalkers-zombies.jpg',
+  keywords: [' ', ' ', ' ', ' '],
+  },
+];
